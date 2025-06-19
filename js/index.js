7816d41cd4393e680e6aa4bb37b589d82027a9b3
Zaher ABBAS <z.abbas83@yahoo.com>
2025-06-19 15:55:34 +0200

    Responive for mobile, tablette, and big screens

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let index = [];
 let buttons = [];
-let choosenProduct = {};
+let chosenProduct = {};
 
 
 
@@ -23,8 +23,8 @@ function getProducts() {
             buttons.forEach(button => button.addEventListener("click", () => {
                 index.forEach(product => {
                     if(product.id === parseInt(button.parentElement.parentElement.id)) {
-                        choosenProduct = product;
-                        window.location.href = `product.html?id=${choosenProduct.id}`;
+                        chosenProduct = product;
+                        window.location.href = `product.html?id=${chosenProduct.id}`;
                     }
                 })
 
@@ -36,21 +36,22 @@ function createProduct(product) {
 let productsList = document.getElementById("products");
 let productFigure = document.createElement("figure");
 let productFigureCaption = document.createElement("figcaption");
+let productImageDiv = document.createElement("div");
 let productImage = document.createElement("img");
 let productPrice = document.createElement("p");
 let productButtonDiv = document.createElement("div");
 let productButton = document.createElement("button");
 
 productFigure.id = product.id.toString();
-productFigureCaption.innerHTML = `<h4>${product.title}</h4>`;
+productFigureCaption.innerHTML = `<h3>${product.title}</h3><h5>${product.brand}</h5>`;
 productImage.src = product.thumbnail;
 productPrice.innerHTML = `<h5>Prix: ${product.price}$</h5>`;
-productButtonDiv.classList.add("button-div");
 productButton.innerHTML = "Voir le produit";
 
 productsList.appendChild(productFigure);
 productFigure.appendChild(productFigureCaption);
-productFigure.appendChild(productImage);
+productFigure.appendChild(productImageDiv);
+productImageDiv.appendChild(productImage);
 productFigure.appendChild(productPrice);
 productFigure.appendChild(productButtonDiv);
 productButtonDiv.appendChild(productButton);
--- js/product.js
+++ js/product.js
@@ -1,6 +1,7 @@
 let products = [];
-let choosenProduct = {};
+let chosenProduct = {};
 let productH2 = document.querySelector("h2");
+let productH4 = document.querySelector("h4");
 let productImage = document.querySelector("img");
 let productDescription = document.querySelector("p");
 
@@ -21,10 +22,11 @@ function getProducts() {
 
             products.forEach(product => {
                 if (product.id == productId) {
-                    choosenProduct = product;
-                    productH2.innerHTML = choosenProduct.title;
-                    productImage.src = `${choosenProduct.images[0]}`;
-                    productDescription.innerHTML = choosenProduct.description;
+                    chosenProduct = product;
+                    productH2.innerHTML = chosenProduct.title;
+                    productH4.innerHTML = chosenProduct.brand;
+                    productImage.src = `${chosenProduct.images[0]}`;
+                    productDescription.innerHTML = chosenProduct.description;
                 }
             });
 
